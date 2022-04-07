1782d01be4b1a981c3775eaca61c41699c353966
Crowdbotics <team@crowdbotics.com>
2022-04-07 06:50:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "hyperfest_34641",
+  url: "https://hyperfest_34641.botics.co",
+  api: "https://hyperfest_34641.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
